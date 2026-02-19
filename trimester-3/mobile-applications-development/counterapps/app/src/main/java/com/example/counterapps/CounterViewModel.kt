ed441bb35939f33ff9abefe7afb3ef4f39b89b47
Ahanya Mariam <ahanyam3@gmail.com>
2026-02-19 22:47:59 +0530

    Counter App using View Models

---
+++ trimester-3/mobile-applications-development/counterapps/app/src/main/java/com/example/counterapps/CounterViewModel.kt
@@ -0,0 +1,22 @@
+package com.example.counterapps
+
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+
+class CounterViewModel : ViewModel() {
+    private val _count = mutableStateOf(0)
+    val count: State<Int> = _count
+
+    fun increment() {
+        _count.value++
+    }
+
+    fun decrement() {
+        _count.value--
+    }
+
+    fun reset() {
+        _count.value = 0
+    }
+}
